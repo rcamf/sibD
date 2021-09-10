5feb8cd298e4ee2d81d40ea76bd05bfa3d0bc41a
David Kühnemann <49859929+qnmn@users.noreply.github.com>
2021-09-10 12:16:42 +0200

    Adjust bbaum-insert to struct

--- bbaum/bbaum.rkt
+++ bbaum/bbaum.rkt
@@ -4,23 +4,25 @@
 
 (define tree0 '((3 8) ((1 2)) ((4 5)) ((6 7))))
 
+(define tree1 '((3 5 7 9)))
+
 (define tree2 '((24)
-                ((14 20)
-                 ((1))
-                 ((15))
-                 ((21 23)))
-                ((28)
-                 ((25 27))
-                 ((30)))))
+		((14 20)
+		 ((1))
+		 ((15))
+		 ((21 23)))
+		((28)
+		 ((25 27))
+		 ((30)))))
 
 (define tree3 '((10 31 44)
-                ((2 3 5))
-                ((11 15 20 22))
-                ((35 40))
-                ((45 51))))
+		((2 3 5))
+		((11 15 20 22))
+		((35 40))
+		((45 51))))
 
-(struct entry (key content)
-	#:transparent)
+(struct entry (key child)
+  #:transparent)
 
 (define (make-leaf-entry x)
   (entry x empty))
@@ -32,83 +34,61 @@
 
 (define (node->bbaum n)
   (match n
-	 [(list leaf-keys) (map make-leaf-entry (append leaf-keys '(+inf.0)))]
-	 [(list keys children ...) (make-bbaum keys (map node->bbaum children))]))
+    [(list leaf-keys) (map make-leaf-entry (append leaf-keys '(+inf.0)))]
+    [(list keys children ...) (make-bbaum keys (map node->bbaum children))]))
 
 (define/match (bbaum->node tree)
-	      [((list (entry keys (list)) ... (entry +inf.0 (list)))) (list keys)]
-	      [((list (entry keys contents) ...)) (list* (drop-right keys 1)
-							 (map bbaum->node contents))])
+  [((list (entry keys (list)) ... (entry +inf.0 (list)))) (list keys)]
+  [((list (entry keys children) ...)) (list* (drop-right keys 1)
+					     (map bbaum->node children))])
 
+; Utility
 (define (splitf-around lst proc)
   (let*-values ([(left others) (splitf-at lst (compose not proc))])
     (values left (first others) (rest others))))
 
-; Inserting:
-(define (split m target)
-  (define child (cdr target))
-  (if (number? child) ; Inserted under leaf node.
-      (list (list child) (list (car target)))
-      (if (<= (length child) (+ 1 (* 2 m)))
-	  (list target)
-	  (let*-values ([(left others) (split-at child m)]
-			[(middle right) (split-at others 1)])
-	    (list (cons (caar middle) (append left (list (cons +inf.0 (cdr middle)))))
-		  (cons (car target) right))))))
+(define (split-around lst pos)
+  (let*-values ([(left others) (split-at lst pos)])
+    (values left (first others) (rest others))))
+
+
+; Insertion
+(define/match (split e m)
+  [((entry key child) m)
+   (if (> (length child) (+ 1 (* 2 m)))
+     (match-let-values ([(pred (entry new-key new-pred-child) succ) (split-around child m)])
+       (list (entry new-key
+		    (append pred (list (entry +inf.0 new-pred-child))))
+	     (entry key succ)))
+     (list e))])
 
 (define (bbaum-insert-rec tree m element)
-  (if (null? tree)
-      element
-      (let*-values ([(left others) (splitf-at tree
-					      (lambda (entry) (> element (first entry))))]
-		    [(target right) (split-at others 1)]
-		    [(divider) (caar target)]
-		    [(new-child) (bbaum-insert-rec (cdar target) m element)])
-	(append left (split m (cons divider new-child)) right))))
+  (match tree
+    [(list (entry keys '()) ...) (map make-leaf-entry (sort (cons element keys) <))]
+    [entries (match-let-values ([(pred (entry target-key target-child) succ)
+				 (splitf-around entries
+						(lambda (e) (<= element (entry-key e))))])
+	       (append pred
+		       (split (entry target-key
+				     (bbaum-insert-rec target-child m element)) m)
+		       succ))] ))
 
 (define (bbaum-insert tree m element)
-  (let ([new-tree (bbaum-insert-rec tree m element)])
-    (if (<= (length new-tree) (+ 1 (* 2 m)))
-	new-tree
-	(split m (cons +inf.0 new-tree)))))
-
-
-(define (balance m t1 t2) empty)
-
-; Deletion:
-(define (bbaum-delete tree m element)
-  (if (= 1 (length (first tree)))
-      (filter-not (lambda (entry) (= element (first entry))) tree)
-      (let*-values ([(index) (- (index-where tree (lambda (x) (<= element (first x)))) 1)] ; TODO: index = -1
-	     [(left others) (split-at tree (max 0 index))]
-	     [(pred) (first others)]
-	     [(target-list right) (split-at (drop others (if (= index -1) 0 1)) 1)]
-	     [(target) (first target-list)]
-	     [(child) (rest target)]
-	     [(new-child) (bbaum-delete child m element)]) ; TODO: key could be element.
-	(cond [(> (length new-child) m) (append left (list pred new-child) right)]
-	      ; Ausgleich links.
-	      ; [(> (length (rest pred) (+ m 1)))
-	      ;  (append left
-		       ; (split m (cons (first new-child)
-				      ; (append (drop-right (rest pred) 1)
-					      ; (list (cons (first pred)
-							  ; (rest (last (rest pred)))))
-					      ; (rest new-child))))
-		       ; right)]
-	      ))))
+  (match (split (entry +inf.0 (bbaum-insert-rec tree m element)) m)
+    [(list (entry +.inf.0 child)) child]
+    [x x]))
 
 
 ; Rendering:
 (define (render-node content)
   (frame (inset
-          (text (string-join (map number->string content)))
-          10)))
+	   (text (string-join (map number->string content)))
+	   10)))
 
 (define (bbaum tree)
   (apply tree-layout
-         (map bbaum (rest tree))
-         #:pict (render-node (first tree)) ))
+	 (map bbaum (rest tree))
+	 #:pict (render-node (first tree)) ))
 
 (define (render tree)
   (naive-layered (bbaum tree)))
@@ -116,18 +96,8 @@
 (define (render-bbaum tree)
   (render (bbaum->node tree)))
 
-; (define (insert-all tree m elements)
-;   (foldl (lambda (e t)
-;            (begin
-;              (show-pict (render t))
-;            (bbaum-insert t m e))) tree elements))
 
-
-; (define b1 (node->bbaum tree3))
-; (render-bbaum b1)
-; (define b2 (bbaum-insert b1 2 9))
-; (render-bbaum b2)
-; (define b3 (bbaum-insert b2 2 14))
-; (render-bbaum b3)
-; (define b4 (bbaum-insert b3 2 8))
-; (render-bbaum b4)
+(define b0 (node->bbaum tree0))
+(define b1 (node->bbaum tree1))
+(define b2 (node->bbaum tree2))
+(define b3 (node->bbaum tree3))
