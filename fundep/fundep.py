f67044cea18c4ffaf688415f9b1656ddd09e5a20
David Kühnemann <49859929+qnmn@users.noreply.github.com>
2021-09-24 20:25:11 +0200

    Add synthesis and decomposition

--- fundep/fundep.py
+++ fundep/fundep.py
@@ -55,7 +55,7 @@ def find_keys(relation, deps):
         else:
             return frozenset()
 
-    return [set(key) for key in rec(frozenset(relation))]
+    return [frozenset(key) for key in rec(frozenset(relation))]
 
 
 def canonical_cover(deps):
@@ -85,8 +85,9 @@ def canonical_cover(deps):
         d = remaining.pop(0)
         left, right = d
         for b in sorted(right):
-            if b in closure(left,
-                            remaining + right_reduced + [Dep(left, right - {b})]):
+            if b in closure(
+                    left,
+                    remaining + right_reduced + [Dep(left, right - {b})]):
                 new_dep = Dep(left, right - {b})
                 print('{} ist rechtsreduzierbar um {} zu {}.'.format(
                     d, b, new_dep))
@@ -107,3 +108,89 @@ def canonical_cover(deps):
                 remaining.remove(dep)
         final.append(Dep(left, new_right))
     return final
+
+
+Rel = namedtuple('Rel', ['name', 'attrs', 'deps'])
+
+# def to2NF(relation, deps):
+#     key_candidates = find_keys(relation, deps)
+#     npas = set(relation) - frozenset.union(*key_candidates)
+#     # Find all true subsets of candidate keys
+#     print(key_candidates)
+#     part_keys = frozenset.union(*(frozenset(powerset(key)) for key in key_candidates)) - frozenset(key_candidates)
+#     return part_keys
+
+
+def synthesize(relation, deps):
+    # Step 1
+    print('Bilden der kanonischen Überdeckung:')
+    fc = canonical_cover(deps)
+    print('Zwischenergebnis: {}\n'.format(fc))
+
+    # Step 2
+    print('Relationenschemata bilden:')
+    rels = []
+    for dep in fc:
+        left, right = dep
+        name = ''.join(itertools.chain(sorted(left), sorted(right)))
+        attrs = left | right
+        fs = [f for f in fc if (f.left | f.right) <= attrs]
+        print('- Aus {} bilde eine Relation R_{} := ({{{}}}, {})'.format(
+            dep, name, ', '.join(sorted(attrs)), fs))
+        rels.append(Rel(name, attrs, fs))
+    print()
+
+    # Step 3
+    keys = find_keys(relation, deps)
+    if not any(key <= r.attrs for r in rels for key in keys):
+        print('Keine Relation enthält einen Schlüsselkandidaten.')
+        key = keys[0]
+        name = ''.join(sorted(key))
+        deps = [Dep(key, key)]
+        attr_str = ', '.join(sorted(key))
+        print(
+            'Erzeuge aus Schlüsselkandidat {{{}}} neue Relation R_{} := ({{{}}}, {})'
+            .format(attr_str, name, attr_str, deps))
+        rels.append(Rel(name, key, deps))
+    print()
+
+    # Step 4
+    for rel in rels.copy():
+        name, a, d = rel
+        conflict = next(
+            (oname
+             for oname, other, _ in rels if oname != name and a <= other),
+            None)
+        if conflict is not None:
+            print(
+                'Die Attribute von R_{} sind in R_{} enthalten; Entferne R_{}'.
+                format(name, conflict, name))
+            rels.remove(rel)
+    print()
+
+    print('Endergebnis:')
+    for name, attr, deps in rels:
+        print(' - R_{} := ({{{}}}, {})'.format(name, ', '.join(sorted(attr)), deps))
+    return rels
+
+
+def decompose(relation, deps):
+    z = [(relation, deps)]
+    in_bcnf = []
+    while z:
+        r, fd = z.pop(0)
+        keys = find_keys(r, fd)
+        for d in fd:
+            if len(d.left.intersection(
+                    d.right)) == 0 and not is_super_key(d.left):
+                x1 = d.left | d.right
+                r1 = (x1, [f for f in fd if (fd.left | fd.right) <= x1])
+                x2 = r - d.right
+                r2 = (x2, [f for f in fd if (fd.left | fd.right) <= x2])
+                # Check if both dependencies add up to fd to see whether the decomposition is lossless.
+                z.append(r1)
+                z.append(r2)
+                break
+        else:
+            in_bcnf.append((r, fd))
+    return in_bcnf
